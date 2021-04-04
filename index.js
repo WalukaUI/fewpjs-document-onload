2b41a8a9ff1088f84f233e6ffc977634d512bfdb
WalukaUI <cwaluka@yahoo.com>
2021-04-03 20:56:30 -0700

    content changed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(e) {
+    let change = document.getElementById('text')
+    change.innerHTML="This is really cool!"
+        
+  });
